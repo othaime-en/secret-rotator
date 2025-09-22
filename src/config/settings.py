df427482979c0bd2edbbbca2cedced0590878d26
othaime-en <61498231+othaime-en@users.noreply.github.com>
2025-09-22 21:58:16 +0300

    initial commit

---
+++ src/config/settings.py
@@ -0,0 +1,12 @@
+import os
+
+class Settings:
+    
+    def load_config(self):
+        """Load configuration from YAML file"""
+    
+    def get(self):
+        """Get configuration value by key"""
+
+# Global settings instance
+settings = Settings()
